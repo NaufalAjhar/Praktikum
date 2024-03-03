b8cfa38f2e8bcad6852a9517a32af37d2fea6aad
NaufalAjhar <74417421+NaufalAjhar@users.noreply.github.com>
2024-03-03 21:47:08 +0700

    tambahan fitur gui

---
+++ Modul3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Modul3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(12, 12);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(299, 22);
+            this.inputBox.TabIndex = 0;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(323, 11);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 1;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // outputText
+            // 
+            this.outputText.AutoSize = true;
+            this.outputText.Location = new System.Drawing.Point(12, 55);
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(42, 16);
+            this.outputText.TabIndex = 3;
+            this.outputText.Text = "Ouput";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 173);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label outputText;
+    }
+}
+
